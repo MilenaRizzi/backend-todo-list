da9f8ab7a265d6bc06808b180319e64d6ea7b26c
Your Name <milenaclararizzi@gmail.com>
2024-04-29 12:12:25 -0300

    Update ExistingRecordException.java

--- src/main/java/com/milenarizzi/todo/exception/ExistingRecordException.java
+++ src/main/java/com/milenarizzi/todo/exception/ExistingRecordException.java
@@ -1,5 +1,8 @@
 package com.milenarizzi.todo.exception;
 
+
 public class ExistingRecordException extends RuntimeException{
-  
+  public ExistingRecordException(String mensagem) {
+    super(mensagem);
+}
 }
