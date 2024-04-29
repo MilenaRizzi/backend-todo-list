7c5a857f291641f008a83e13bf673e48b55374c1
Your Name <milenaclararizzi@gmail.com>
2024-04-29 10:39:18 -0300

    Update TodoApplication.java

--- src/main/java/com/milenarizzi/todo/TodoApplication.java
+++ src/main/java/com/milenarizzi/todo/TodoApplication.java
@@ -1,7 +1,9 @@
 package com.milenarizzi.todo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoApplication {
@@ -10,4 +12,8 @@ public class TodoApplication {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
+    @Bean
+    ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
 }
