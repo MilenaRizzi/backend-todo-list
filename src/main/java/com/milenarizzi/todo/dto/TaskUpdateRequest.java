e7b2445d7b0ef6ee439aecc8fcf52e18e15bdcc3
Your Name <milenaclararizzi@gmail.com>
2024-04-29 12:12:23 -0300

    Update TaskUpdateRequest.java

--- src/main/java/com/milenarizzi/todo/dto/TaskUpdateRequest.java
+++ src/main/java/com/milenarizzi/todo/dto/TaskUpdateRequest.java
@@ -1,13 +1,11 @@
 package com.milenarizzi.todo.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+@Data
 public class TaskUpdateRequest {
   
-  @NotEmpty(message = "Campo obrigatório")
   private String description;
 
-  @NotNull
   private boolean completed;  
 }
