70c235dc3986bf09cd5375cdd13c00260f3b88b2
Your Name <milenaclararizzi@gmail.com>
2024-04-29 12:12:18 -0300

    Update TaskRequest.java

--- src/main/java/com/milenarizzi/todo/dto/TaskRequest.java
+++ src/main/java/com/milenarizzi/todo/dto/TaskRequest.java
@@ -1,15 +1,15 @@
 package com.milenarizzi.todo.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class TaskRequest {
 
   @NotEmpty(message = "Campo obrigatório")
+  @Size(min = 3, max = 200)
   private String description;
 
-  @NotNull
   private boolean completed;
 }
